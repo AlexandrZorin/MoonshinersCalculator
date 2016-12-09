5f59477de8a6510dd02e9d825e201ec9bd0bec65
Zorin Alexandr <evol-e@mail.ru>
2016-12-09 19:57:15 +0500

    create project, first commit

---
+++ app/src/main/java/project/phoenix/moonshiterscalculator/MainActivity.java
@@ -0,0 +1,26 @@
+package project.phoenix.moonshiterscalculator;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+    String[] names = {"fff", "ddd", "add", "sdd", "gdd", "cdd", "edd"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.rowlayout, R.id.label, names);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
+}
