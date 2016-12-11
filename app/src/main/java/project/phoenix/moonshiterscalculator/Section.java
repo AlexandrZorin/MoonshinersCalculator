f591b4a70beea585d80a13513aac1db2e90aac1f
Phoenix <Evol-e@mail.ru>
2016-12-11 14:01:04 +0500

    add class Section

---
+++ app/src/main/java/project/phoenix/moonshiterscalculator/Section.java
@@ -0,0 +1,37 @@
+package project.phoenix.moonshiterscalculator;
+
+public class Section {
+    private String sectionName;
+    private String sectionDescription;
+    private int image;
+
+    public Section(String sectionName, String sectionDescription, int image) {
+        this.sectionName = sectionName;
+        this.sectionDescription = sectionDescription;
+        this.image = image;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public String getSectionDescription() {
+        return sectionDescription;
+    }
+
+    public void setSectionDescription(String sectionDescription) {
+        this.sectionDescription = sectionDescription;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
