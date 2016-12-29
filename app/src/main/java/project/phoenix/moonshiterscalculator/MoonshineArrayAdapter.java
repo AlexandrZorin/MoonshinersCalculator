5bb07f65a30a2375c8b69d492321c9d9fbae96ec
Zorin Alexandr <evol-e@mail.ru>
2016-12-29 10:35:18 +0500

    add comments

--- app/src/main/java/project/phoenix/moonshiterscalculator/MoonshineArrayAdapter.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/MoonshineArrayAdapter.java
@@ -12,23 +12,40 @@ import android.widget.TextView;
 
 import java.util.List;
 
+/**
+ * Custom ListView adapter based on ArrayAdapter.
+ * This class expects that the provided objects list which have at least three fields
+ * two String and one int for transmission resources in title, description and image.
+ */
+
 public class MoonshineArrayAdapter extends ArrayAdapter {
     private final Context context;
-    private final List<Section> sections;
+    private final List<Object> sections;
 
-    public MoonshineArrayAdapter(Context context, List<Section> sections) {
+    /**
+     * Constructor
+     *
+     * @param context The current context.
+     * @param sections Objects list which have fields for title, description and image.
+     */
+    public MoonshineArrayAdapter(Context context, List<Object> sections) {
         super(context, R.layout.rowlayout, sections);
         this.context = context;
         this.sections = sections;
     }
 
+    /**
+     * Returns the item of the position in the list.
+     *
+     * @param position The position to retrieve the item of.
+     *
+     * @return The item of the position.
+     */
     @Nullable
     @Override
     public Object getItem(int position) {
         return sections.get(position);
     }
-
-
 
     @NonNull
     @Override
@@ -42,7 +59,7 @@ public class MoonshineArrayAdapter extends ArrayAdapter {
         }
 
         Section section = getSection(position);
-        ((TextView) rowView.findViewById(R.id.label)).setText(section.getSectionName());
+        ((TextView) rowView.findViewById(R.id.title)).setText(section.getSectionName());
         ((TextView) rowView.findViewById(R.id.description)).setText(section.getSectionDescription());
         ((ImageView) rowView.findViewById(R.id.image)).setImageResource(section.getImage());
         return rowView;
