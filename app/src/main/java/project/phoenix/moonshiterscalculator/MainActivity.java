c9d08f030fe69251e1581ca83275f09af43ac4ab
Zorin Alexandr <evol-e@mail.ru>
2016-12-29 15:11:24 +0500

    refactoring class and add quit menu

--- app/src/main/java/project/phoenix/moonshiterscalculator/MainActivity.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/MainActivity.java
@@ -1,33 +1,87 @@
 package project.phoenix.moonshiterscalculator;
 
-import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends ListActivity {
-    ArrayList<Object> sections = new ArrayList<>();
+import project.phoenix.moonshiterscalculator.sectionone.SectionOneActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<Object> sections = new ArrayList<>();
+    private MoonshineArrayAdapter listAdapter;
+    private ListView listView;
+    private Context context = this;
     {
         sections.add(new Section("Раздел 1", "описание", R.mipmap.ic_launcher));
-        sections.add(new Section("Раздел 1", "описание", R.mipmap.ic_launcher));
-        sections.add(new Section("Раздел 1", "описание", R.mipmap.ic_launcher));
-        sections.add(new Section("Раздел 1", "описание", R.mipmap.ic_launcher));
-        sections.add(new Section("Раздел 1", "описание", R.mipmap.ic_launcher));
+        sections.add(new Section("Раздел 2", "описание", R.mipmap.ic_launcher));
+        sections.add(new Section("Раздел 3", "описание", R.mipmap.ic_launcher));
+        sections.add(new Section("Раздел 4", "описание", R.mipmap.ic_launcher));
+        sections.add(new Section("Раздел 5", "описание", R.mipmap.ic_launcher));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MoonshineArrayAdapter adapter = new MoonshineArrayAdapter(this, sections);
-        setListAdapter(adapter);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        listViewCreate();
     }
 
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        Toast.makeText(this, "selected", Toast.LENGTH_LONG).show();
+    public void onBackPressed() {
+        dialogQuitApplication();
+    }
+
+    private void listViewCreate() {
+        listAdapter = new MoonshineArrayAdapter(context, sections);
+        listView = (ListView) findViewById(R.id.mainList);
+        listView.setAdapter(listAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onListViewItemClick(position);
+            }
+        });
+    }
+
+    private void onListViewItemClick(int position) {
+        if (position == 0) {
+            Intent intent = new Intent(MainActivity.this, SectionOneActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(context, "selected " + (position + 1) + " element", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void dialogQuitApplication() {
+        AlertDialog.Builder quitDialog = new AlertDialog.Builder(MainActivity.this);
+        quitDialog.setTitle("Выход: Вы уверены?");
+        quitDialog.setPositiveButton("Да!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+        quitDialog.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // TODO Auto-generated method stub
+            }
+        });
+        quitDialog.show();
     }
 }
