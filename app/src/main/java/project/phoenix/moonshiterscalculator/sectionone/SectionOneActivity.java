16a6d8f59358f81710d290ab8b0c085db191c718
Zorin Alexandr <evol-e@mail.ru>
2016-12-29 15:10:08 +0500

    add SectionOneActivity

---
+++ app/src/main/java/project/phoenix/moonshiterscalculator/sectionone/SectionOneActivity.java
@@ -0,0 +1,32 @@
+package project.phoenix.moonshiterscalculator.sectionone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import project.phoenix.moonshiterscalculator.MainActivity;
+
+public class SectionOneActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            startActivity(new Intent(this, MainActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
