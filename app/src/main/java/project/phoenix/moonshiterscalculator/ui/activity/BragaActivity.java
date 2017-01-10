c00535bc56ed0de2ea96db469e1a6b2930c3923f
Zorin Alexandr <evol-e@mail.ru>
2017-01-10 19:56:06 +0500

    add parserStringToDoubleResources

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/BragaActivity.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/BragaActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -20,12 +22,21 @@ public class BragaActivity extends AppCompatActivity {
     private EditText editTextWaterVolume;
     private TextView textViewMaxStrength;
     private TextView textViewVolumeSolution;
+    private double weightSugar;
+    private double waterVolume;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_braga);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initViews();
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -49,6 +60,19 @@ public class BragaActivity extends AppCompatActivity {
         editTextWaterVolume = (EditText) findViewById(R.id.braga_water_volume);
         textViewMaxStrength = (TextView) findViewById(R.id.braga_max_strength);
         textViewVolumeSolution = (TextView) findViewById(R.id.braga_volume_solution);
+    }
+
+    private void parserStringToDoubleForResources() {
+        String textWeightSugar = editTextWeightSugar.getText().toString();
+        String textWaterVolume = editTextWaterVolume.getText().toString();
+        if (textWeightSugar.matches("")) {
+            Toast.makeText(this, R.string.braga_exception_blank_field_weight_sugar, Toast.LENGTH_LONG).show();
+        } else if (textWaterVolume.matches("")) {
+            Toast.makeText(this, R.string.braga_exception_blank_field_water_volume, Toast.LENGTH_LONG).show();
+        } else {
+            weightSugar = Double.parseDouble(textWeightSugar);
+            waterVolume = Double.parseDouble(textWaterVolume);
+        }
     }
 
     private double dehydratedAlcohol(double sugarMg) {
