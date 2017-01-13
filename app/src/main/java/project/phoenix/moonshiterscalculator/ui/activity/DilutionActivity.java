f3324b02a22f2dfc78145ba15ff8185933fb39c6
Zorin Alexandr <evol-e@mail.ru>
2017-01-13 12:16:38 +0500

    Optimize Imports

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/DilutionActivity.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/DilutionActivity.java
@@ -1,10 +1,7 @@
 package project.phoenix.moonshiterscalculator.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
