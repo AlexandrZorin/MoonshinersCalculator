200907902a503f85c70e2df8a8e62e7d74edb215
Zorin Alexandr <evol-e@mail.ru>
2017-01-27 18:50:41 +0500

    add dao class

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/db/dao/StrengthCorrection.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/db/dao/StrengthCorrection.java
@@ -1,4 +1,20 @@
 package project.phoenix.moonshiterscalculator.ui.db.dao;
 
+import android.content.Context;
+
+import project.phoenix.moonshiterscalculator.ui.db.MoonshineDBHelper;
+
 public class StrengthCorrection {
+    String strengthAreometer;
+    String temperature;
+    Context context;
+    MoonshineDBHelper moonshineDBHelper;
+
+    public StrengthCorrection(Context context) {
+        this.context = context;
+    }
+
+    private void initMoonshineDBHelper() {
+        moonshineDBHelper = new MoonshineDBHelper(context);
+    }
 }
