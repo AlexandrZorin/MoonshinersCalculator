459a192588735e20dfc48ef5088d3fcba3729450
Zorin Alexandr <evol-e@mail.ru>
2017-01-27 18:14:30 +0500

    testing select

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/StrengthCorrectActivity.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/StrengthCorrectActivity.java
@@ -2,16 +2,30 @@ package project.phoenix.moonshiterscalculator.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import project.phoenix.moonshiterscalculator.R;
 import project.phoenix.moonshiterscalculator.ui.activity.template.TemplateActivity;
+import project.phoenix.moonshiterscalculator.ui.db.MoonshineDBHelper;
 
 
 public class StrengthCorrectActivity extends TemplateActivity {
+    private ListView obj;
+    MoonshineDBHelper moonshineDBHelper;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_strength_correct);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        moonshineDBHelper = new MoonshineDBHelper(this);
+        ArrayList arrayList = moonshineDBHelper.getAllTemperature();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        obj = (ListView) findViewById(R.id.listView);
+        obj.setAdapter(arrayAdapter);
     }
 }
