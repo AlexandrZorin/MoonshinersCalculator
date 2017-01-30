0e142170d17298296542a00c59b34f006ebd2fc7
Phoenix <Evol-e@mail.ru>
2017-01-30 21:45:25 +0500

    add logic; add interaction with database

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/StrengthCorrectActivity.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/StrengthCorrectActivity.java
@@ -1,9 +1,16 @@
 package project.phoenix.moonshiterscalculator.ui.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.ContextThemeWrapper;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,18 +20,51 @@ import project.phoenix.moonshiterscalculator.ui.db.MoonshineDBHelper;
 
 
 public class StrengthCorrectActivity extends TemplateActivity {
+    private Button buttonResult;
+    private EditText editTextAreometerStrength;
+    private EditText editTextTemperature;
+    private TextView textViewCorrectStrength;
+    private Context context = this;
     private ListView obj;
-    MoonshineDBHelper moonshineDBHelper;
+    private MoonshineDBHelper moonshineDBHelper;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_strength_correct);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initViews();
         moonshineDBHelper = new MoonshineDBHelper(this);
-        ArrayList arrayList = moonshineDBHelper.getAllTemperature();
+        buttonResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textAreometerStrength = editTextAreometerStrength.getText().toString();
+                String textTemperature = editTextTemperature.getText().toString();
+                if (textAreometerStrength.matches("")) {
+                    Toast.makeText(context, "fff", Toast.LENGTH_LONG);
+                } else if (textTemperature.matches("")) {
+                    Toast.makeText(context, "ddd", Toast.LENGTH_LONG);
+                } else {
+                    String string = moonshineDBHelper.getCorrectStrength(textTemperature, textAreometerStrength);
+                    textViewCorrectStrength.setText(string);
+                }
+            }
+        });
+
+        /*ArrayList arrayList = moonshineDBHelper.getAllTemperature();
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
         obj = (ListView) findViewById(R.id.listView);
-        obj.setAdapter(arrayAdapter);
+        obj.setAdapter(arrayAdapter);*/
+    }
+
+    private void initViews() {
+        buttonResult = (Button)
+                findViewById(R.id.strength_correct_button);
+        editTextAreometerStrength = (EditText)
+                findViewById(R.id.strength_correct_areometer_strength);
+        editTextTemperature = (EditText)
+                findViewById(R.id.strength_correct_temperature);
+        textViewCorrectStrength  = (TextView)
+                findViewById(R.id.strength_correct);
     }
 }
