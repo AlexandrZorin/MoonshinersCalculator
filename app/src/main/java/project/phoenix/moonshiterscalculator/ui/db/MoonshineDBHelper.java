291abd96d810f9efcefb1fdc99a5aac4ebe4f5d5
Zorin Alexandr <evol-e@mail.ru>
2017-02-02 15:31:17 +0500

    add all needed queries

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/db/MoonshineDBHelper.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/db/MoonshineDBHelper.java
@@ -20,21 +20,86 @@ public class MoonshineDBHelper extends SQLiteAssetHelper {
         database = this.getReadableDatabase();
         return database.rawQuery(
                 "SELECT correct_strength " +
-                "FROM areometer_strength, table_temperature " +
-                "JOIN correct_strength_with_temperature ON areometer_strength._id = " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
                         "correct_strength_with_temperature._id_areometer_strength " +
                 "AND table_temperature._id = " +
                         "correct_strength_with_temperature._id_table_temperature " +
-                "WHERE strength = ? AND temperature = ?;",
-                new String[]{areometerStrength, temperature});
+                "AND strength = ? " +
+                "AND temperature = ?;",
+                new String[] {areometerStrength, temperature}
+        );
     }
 
     public Cursor getRoundingAreometerStrength(String areometerStrength, String temperature) {
         database = this.getReadableDatabase();
         return database.rawQuery(
-                "SELECT temperature, areometer_strength, correct_strength " +
-                "FROM ",
-                null
+                "SELECT temperature, MAX(strength) AS strength, correct_strength " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
+                        "correct_strength_with_temperature._id_areometer_strength " +
+                "AND table_temperature._id = " +
+                        "correct_strength_with_temperature._id_table_temperature " +
+                "AND strength < " + areometerStrength + " " +
+                "AND temperature = " + temperature + " " +
+                "UNION " +
+                "SELECT temperature, MIN(strength) AS strength, correct_strength " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
+                        "correct_strength_with_temperature._id_areometer_strength " +
+                "AND table_temperature._id = " +
+                        "correct_strength_with_temperature._id_table_temperature " +
+                "AND strength > ? " +
+                "AND temperature = ?;",
+                new String[] {areometerStrength, temperature}
+        );
+    }
+
+    public Cursor getRoundingTemperature(String areometerStrength, String temperature) {
+        database = this.getReadableDatabase();
+        return database.rawQuery(
+                "SELECT MAX(temperature) AS temperature, strength, correct_strength " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
+                        "correct_strength_with_temperature._id_areometer_strength " +
+                "AND table_temperature._id = " +
+                        "correct_strength_with_temperature._id_table_temperature " +
+                "AND strength = " + areometerStrength + " " +
+                "AND temperature < " + temperature + " " +
+                "UNION " +
+                "SELECT MIN(temperature), strength, correct_strength " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
+                        "correct_strength_with_temperature._id_areometer_strength " +
+                "AND table_temperature._id = " +
+                        "correct_strength_with_temperature._id_table_temperature " +
+                "AND strength = ? " +
+                "AND temperature > ?;",
+                new String[] {areometerStrength, temperature}
+        );
+    }
+
+    public Cursor getRoundingAll(String areometerStrength, String temperature) {
+        database = this.getReadableDatabase();
+        return database.rawQuery(
+                "SELECT MAX(temperature), MAX(strength) AS strength, correct_strength " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
+                        "correct_strength_with_temperature._id_areometer_strength " +
+                "AND table_temperature._id = " +
+                        "correct_strength_with_temperature._id_table_temperature " +
+                "AND strength < " + areometerStrength + " " +
+                "AND temperature < " + temperature + " " +
+                "UNION " +
+                "SELECT MIN(temperature), MIN(strength) AS strength, correct_strength " +
+                "FROM table_temperature, areometer_strength, correct_strength_with_temperature " +
+                "WHERE areometer_strength._id = " +
+                        "correct_strength_with_temperature._id_areometer_strength " +
+                "AND table_temperature._id = " +
+                        "correct_strength_with_temperature._id_table_temperature " +
+                "AND strength > ? " +
+                "AND temperature > ?;",
+                new String[] {areometerStrength, temperature}
         );
     }
 }
