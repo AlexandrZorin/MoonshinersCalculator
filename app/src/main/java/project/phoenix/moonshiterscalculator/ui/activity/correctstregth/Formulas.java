eff025d2551078d1f51d9084cd6cc3e842a3dc69
Zorin Alexandr <evol-e@mail.ru>
2017-02-20 18:13:20 +0500

    refactoring

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/correctstregth/Formulas.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/correctstregth/Formulas.java
@@ -2,17 +2,58 @@ package project.phoenix.moonshiterscalculator.ui.activity.correctstregth;
 
 public class Formulas {
 
-    public double formulaRoundingAreometerStrength(double x, double x1, double x2, double x1y, double x2y) {
-        return x2y - (((x2 - x) / (x2 - x1)) * (x2y - x1y));
+    public double formulaRoundingAreometerStrength(
+            double areometerStrength,
+            double areometerStrengthRoundDown,
+            double areometerStrengthRoundUp,
+            double correctStrengthRoundDown,
+            double correctStrengthRoundUp
+    )
+    {
+        return correctStrengthRoundUp -
+                (((areometerStrengthRoundUp - areometerStrength) /
+                        (areometerStrengthRoundUp - areometerStrengthRoundDown)) *
+                        (correctStrengthRoundUp - correctStrengthRoundDown));
     }
 
-    public double formulaRoundingTemperature(double temperature, double temperatureRoundDown, double temperatureRoundUp, double correctStrengthRoundDown, double correctStrengthRoundUp) {
-        return correctStrengthRoundDown - (((temperatureRoundDown - temperature) / (temperatureRoundDown - temperatureRoundUp)) * (correctStrengthRoundDown - correctStrengthRoundUp));
+    public double formulaRoundingTemperature(
+            double temperature,
+            double temperatureRoundDown,
+            double temperatureRoundUp,
+            double correctStrengthRoundDown,
+            double correctStrengthRoundUp
+    )
+    {
+        return correctStrengthRoundDown -
+                (((temperatureRoundDown - temperature) /
+                        (temperatureRoundDown - temperatureRoundUp)) *
+                        (correctStrengthRoundDown - correctStrengthRoundUp));
     }
 
-    public double formulaRoundingAll(double x, double x1, double x2, double x1y, double x2y, double x1y2, double x2y2) {
-        double a = x1y2 - (((x1 - x) / (x1 - x2)) * (x1y2 - x2y2));
-        double b = x1y - (((x1 - x) / (x1 - x2)) * (x1y - x2y));
-        return a - (((x2 - x) / (x2 - x1)) * (a - b));
+    public double formulaRoundingAll(
+            double temperature,
+            double areometerStrength,
+            double temperatureRoundDown,
+            double temperatureRoundUp,
+            double areometerStrengthRoundDown,
+            double areometerStrengthRoundUp,
+            double correctStrengthArStrUpTempDown,
+            double correctStrengthArStrUpTempUp,
+            double correctStrengthArStrDownTempDown,
+            double correctStrengthArStrDownTempUp
+    )
+    {
+        double a = correctStrengthArStrDownTempDown -
+                (((temperatureRoundDown - temperature) /
+                        (temperatureRoundDown - temperatureRoundUp)) *
+                        (correctStrengthArStrDownTempDown - correctStrengthArStrDownTempUp));
+        double b = correctStrengthArStrUpTempDown -
+                (((temperatureRoundDown - temperature) /
+                        (temperatureRoundDown - temperatureRoundUp)) *
+                        (correctStrengthArStrUpTempDown - correctStrengthArStrUpTempUp));
+        return a -
+                (((areometerStrengthRoundUp - areometerStrength) /
+                        (areometerStrengthRoundUp - areometerStrengthRoundDown)) *
+                        (a - b));
     }
 }
