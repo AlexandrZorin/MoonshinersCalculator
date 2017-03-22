63e33a1629805cd14e59e81e0e79dd01971e3468
Zorin Alexandr <evol-e@mail.ru>
2017-03-22 15:39:42 +0500

    add mockito

---
+++ app/src/test/java/project/phoenix/moonshiterscalculator/ExampleMockTest.java
@@ -0,0 +1,9 @@
+package project.phoenix.moonshiterscalculator;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExampleMockTest {
+    
+}
