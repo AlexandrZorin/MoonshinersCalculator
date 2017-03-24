5946f4eb6cd0aa2bbdfd609dd8123d6744f8fb1d
Zorin Alexandr <evol-e@mail.ru>
2017-03-24 18:06:16 +0500

    fixed test

--- app/src/test/java/project/phoenix/moonshiterscalculator/ExampleMockitoTest.java
+++ app/src/test/java/project/phoenix/moonshiterscalculator/ExampleMockitoTest.java
@@ -1,25 +1,33 @@
 package project.phoenix.moonshiterscalculator;
 
 import android.content.Context;
-import android.test.mock.MockContext;
+import android.database.Cursor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import project.phoenix.moonshiterscalculator.ui.activity.correctstregth.DeterminationResultCorrectStrength;
+import project.phoenix.moonshiterscalculator.ui.db.MoonshineDBHelper;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ExampleMockTest {
+    @Mock
+    Context mockContext;
+
+    @Mock
+    DeterminationResultCorrectStrength mockDetermTest;
+
+    /**
+     * Useless test :)
+     */
     @Test
     public void testDeterm() {
-        DeterminationResultCorrectStrength mockDetermTest = Mockito.mock(DeterminationResultCorrectStrength.class);
-        Context falseContext = Mockito.mock(MockContext.class);
-        when(mockDetermTest.determResult("67.6", "22.4", falseContext)).thenReturn("67.09");
-        assertEquals("67.09", mockDetermTest.determResult("67.6", "22.4", falseContext));
+        when(mockDetermTest.determResult("67.6", "22.4", mockContext)).thenReturn("67.09");
+        assertEquals("67.09", mockDetermTest.determResult("67.6", "22.4", mockContext));
     }
 }
