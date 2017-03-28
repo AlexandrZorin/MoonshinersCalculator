d442f16d6f00719a13c65a964f88ddb91acccfa9
Zorin Alexandr <evol-e@mail.ru>
2017-03-28 15:11:22 +0500

    added comments

--- app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/correctstregth/DeterminationResultCorrectStrength.java
+++ app/src/main/java/project/phoenix/moonshiterscalculator/ui/activity/correctstregth/DeterminationResultCorrectStrength.java
@@ -7,7 +7,21 @@ import java.util.Locale;
 
 import project.phoenix.moonshiterscalculator.ui.db.MoonshineDBHelper;
 
+/**
+ * Класс для определения скорректированной крепости спиртового раствора.
+ * В классе вытаскиваются нужные данные из базы данных, если введенные пользователем данные
+ * не ровные, они округляются и ответ пересчитывается по формулам вынесенным в отдельный класс.
+ */
+
 public class DeterminationResultCorrectStrength {
+
+    /**
+     * Метод для определения и возврата результата в виде строки.
+     * @param areometerStrength Крепость спиртового раствора показанная ареометром.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @param context Контекст.
+     * @return Возврат вычисленного результата в виде строки.
+     */
     public String determResult(String areometerStrength, String temperature, Context context) {
         if (checkNumberAreometerStrength(areometerStrength) &&
                 checkNumberTemperature(temperature)) {
@@ -39,36 +53,76 @@ public class DeterminationResultCorrectStrength {
         }
     }
 
+    /**
+     * Метод проверяет введенные данные пользователем на соответствие ровности
+     * (ровным значением в данном случае является целое или 0.5).
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @return Возвращает значение true если значение ровное, false если значение не ровное.
+     */
     private boolean checkNumberAreometerStrength(String areometerStrength) {
         double areometerStrengthDouble = Double.parseDouble(areometerStrength);
         return areometerStrengthDouble % 1 == 0 || areometerStrengthDouble % 1 == 0.5;
     }
 
+    /**
+     * Метод проверяет введенные данные пользователем на соответствие ровности
+     * (ровным значением в данном случае является только целое)
+     * @param temperature Температура спиртового раствора во время замеров крепости ареометром.
+     * @return Возвращает значение true если значение ровное, false если значение не ровное.
+     */
     private boolean checkNumberTemperature(String temperature) {
         double temperatureDouble = Double.parseDouble(temperature);
         return temperatureDouble % 1 == 0;
     }
 
+    /**
+     * Округление крепости спиртового раствора до 0.5 в большую сторону.
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @return Возвращает округленное значение в виде строки.
+     */
     private String areometerStrengthRoundUp(String areometerStrength) {
         double doubleAreometerStrength = Double.parseDouble(areometerStrength);
         double result = Math.ceil(doubleAreometerStrength * 2) / 2;
         return String.valueOf(result);
     }
 
+    /**
+     * Округление крепости спиртового раствора до 0.5 в меньшую сторону.
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @return Возвращает округленное значение в виде строки.
+     */
     private String areometerStrengthRoundDown(String areometerStrength) {
         double doubleAreometerStrength = Double.parseDouble(areometerStrength);
         double result = Math.floor(doubleAreometerStrength * 2) / 2;
         return String.valueOf(result);
     }
 
+    /**
+     * Округление температуры спиртового раствора до целого в большую сторону.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @return Возвращает округленное значение в виде строки.
+     */
     private String temperatureRoundUp(String temperature) {
         return String.valueOf(Math.ceil(Double.parseDouble(temperature)));
     }
 
+    /**
+     * Округление температуры спиртового раствора до целого в меньшую сторону.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @return Возвращает округленное значение в виде строки.
+     */
     private String temperatureRoundDown(String temperature) {
         return String.valueOf(Math.floor(Double.parseDouble(temperature)));
     }
 
+    /**
+     * Определение скорректированной крепости спиртового раствора, если все введенные данные
+     * оказались ровными.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @param context Контекст.
+     * @return Возвращает скорректированную крепость спиртового раствора в виде строки.
+     */
     private String determCorrectStrengthWithoutRound
             (String temperature, String areometerStrength, Context context) {
         String cursorCorrectStrength = "";
@@ -86,6 +140,14 @@ public class DeterminationResultCorrectStrength {
         return cursorCorrectStrength;
     }
 
+    /**
+     * Определение скорректированной крепости спиртового раствора, если ровным оказалась только
+     * введенная температура, а крепость показанную ареометром пришлось округлять.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @param context Контекст.
+     * @return Возвращает скорректированную крепость спиртового раствора в виде double.
+     */
     private double determRoundAreometerStrength
             (String temperature, String areometerStrength, Context context) {
         String cursorAreometerStrengthRoundUp = "";
@@ -128,6 +190,14 @@ public class DeterminationResultCorrectStrength {
         return result;
     }
 
+    /**
+     * Определение скорректированной крепости спиртового раствора, если ровным оказалась только
+     * введенная крепость спиртового раствора, а температуру пришлось округлять.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @param context Контекст.
+     * @return Возвращает скорректированную крепость спиртового раствора в виде double.
+     */
     private double determRoundTemperature
             (String temperature, String areometerStrength, Context context) {
         String cursorTemperatureRoundUp = "";
@@ -171,6 +241,14 @@ public class DeterminationResultCorrectStrength {
         return result;
     }
 
+    /**
+     * Определение скорректированной крепости спиртового раствора, округлять пришлось и
+     * температуру и крепость спиртового раствора.
+     * @param temperature Температура спиртового раствора во время измерений ареометром.
+     * @param areometerStrength Крепость спиртового раствора определенная ареометром.
+     * @param context Контекст.
+     * @return Возвращает скорректированную крепость спиртового раствора в виде double.
+     */
     private double determRoundAll
             (String temperature, String areometerStrength, Context context) {
         String cursorTemperatureRoundUp = "";
