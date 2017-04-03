c7092952d9bb16e94be349965f1e9873f70f3170
Zorin Alexandr <evol-e@mail.ru>
2017-04-03 17:00:48 +0500

    added recipes activity

--- app/src/main/java/project/phoenix/moonshinerscalculator/ui/activity/recipes/RecipesActivity.java
+++ app/src/main/java/project/phoenix/moonshinerscalculator/ui/activity/recipes/RecipesActivity.java
@@ -1,4 +1,29 @@
 package project.phoenix.moonshinerscalculator.ui.activity.recipes;
 
-public class RecipesActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import project.phoenix.moonshinerscalculator.R;
+import project.phoenix.moonshinerscalculator.ui.activity.template.TemplateActivity;
+
+public class RecipesActivity extends TemplateActivity {
+    private TextView textViewId;
+    private TextView textViewRecipeName;
+    private TextView textViewRecipeDescriprion;
+    private TextView textViewRecipeText;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initViews();
+    }
+
+    private void initViews() {
+        textViewId = (TextView) findViewById(R.id.recipes_id);
+        textViewRecipeName = (TextView) findViewById(R.id.recipes_recipeName);
+        textViewRecipeDescriprion = (TextView) findViewById(R.id.recipes_recipeDescription);
+        textViewRecipeText = (TextView) findViewById(R.id.recipes_recipeText);
+    }
 }
