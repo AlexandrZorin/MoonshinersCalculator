3e5765c8c1a6a0bbf7f94a4a997d640da2e3a31f
Zorin Alexandr <evol-e@mail.ru>
2017-04-03 14:01:22 +0500

    A class for parsing json from gson was created

--- app/src/main/java/project/phoenix/moonshinerscalculator/datamodel/Recipe.java
+++ app/src/main/java/project/phoenix/moonshinerscalculator/datamodel/Recipe.java
@@ -1,4 +1,40 @@
 package project.phoenix.moonshinerscalculator.datamodel;
 
 public class Recipe {
+    private int id;
+    private String recipeName;
+    private String recipeDescription;
+    private String recipeText;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getRecipeDescription() {
+        return recipeDescription;
+    }
+
+    public void setRecipeDescription(String recipeDescription) {
+        this.recipeDescription = recipeDescription;
+    }
+
+    public String getRecipeText() {
+        return recipeText;
+    }
+
+    public void setRecipeText(String recipeText) {
+        this.recipeText = recipeText;
+    }
 }
