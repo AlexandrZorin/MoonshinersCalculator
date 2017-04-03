56309032ee51e5c23732d099a7f7d126343293a2
Zorin Alexandr <evol-e@mail.ru>
2017-04-03 13:53:39 +0500

    added intent service to parse json

--- app/src/main/java/project/phoenix/moonshinerscalculator/services/RecipesIntentService.java
+++ app/src/main/java/project/phoenix/moonshinerscalculator/services/RecipesIntentService.java
@@ -3,6 +3,14 @@ package project.phoenix.moonshinerscalculator.services;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,8 +19,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import project.phoenix.moonshinerscalculator.datamodel.Recipe;
+
 
 public class RecipesIntentService extends IntentService {
+    private static final String LOG_TAG = "myLog";
+
     public RecipesIntentService(String name) {
         super(name);
     }
@@ -22,26 +34,33 @@ public class RecipesIntentService extends IntentService {
 
     }
 
-    private String jsonParser() {
+    private String jsonUrlToString(String recipeId) {
         HttpURLConnection urlConnection;
         BufferedReader reader;
         String resultJson = "";
         try {
-            URL url = new URL("192.168.101.220");
+            URL url = new URL("http://192.168.101.220:8182/SpringRest/recipes/" + recipeId);
             urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
             InputStream inputStream = urlConnection.getInputStream();
-            StringBuffer buffer = new StringBuffer();
+            StringBuilder builder = new StringBuilder();
             reader = new BufferedReader(new InputStreamReader(inputStream));
             String line;
             while ((line = reader.readLine()) != null) {
-                buffer.append(line);
+                builder.append(line);
             }
-            resultJson = buffer.toString();
+            resultJson = builder.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return resultJson;
+    }
+
+    private void jsonParser(String strJson) {
+        Log.d(LOG_TAG, strJson);
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Recipe recipe = gson.fromJson(strJson, Recipe.class);
     }
 }
