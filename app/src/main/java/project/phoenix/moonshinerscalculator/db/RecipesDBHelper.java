5b5bafecafd974aafcc2786649a7693316256c28
Zorin Alexandr <evol-e@mail.ru>
2017-04-06 18:50:07 +0500

    added helper for database recipes

--- app/src/main/java/project/phoenix/moonshinerscalculator/db/RecipesDBHelper.java
+++ app/src/main/java/project/phoenix/moonshinerscalculator/db/RecipesDBHelper.java
@@ -1,4 +1,25 @@
 package project.phoenix.moonshinerscalculator.db;
 
-public class RecipesDBHelper {
+import android.content.Context;
+import android.database.Cursor;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import project.phoenix.moonshinerscalculator.datamodel.Recipe;
+
+public class RecipesDBHelper extends SQLiteAssetHelper {
+    private static final String DATABASE_NAME = "recipes.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public RecipesDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void addRecipe(Recipe recipe) {
+
+    }
+
+    public Cursor getRecipe(Recipe recipe) {
+        return null;
+    }
 }
